450a9165f494c0c22bcea62e696ba18f05bf737c
ainiderenxm <277407493@qq.com>
2016-12-20 11:48:46 +0800

    init

---
+++ src/Mock.ts
@@ -0,0 +1,16 @@
+export interface MockInter{
+    getUrl(url:string):string;
+}
+
+export default class Mock interface MockInter{
+
+    constructor(){
+
+    }
+
+    getUrl(url:string ):string{
+
+        return '';
+    }
+
+}
